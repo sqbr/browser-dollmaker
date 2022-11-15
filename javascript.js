eb479a319c3856cb534435b3960408cd683382c4
sqbr <sqbr@users.noreply.github.com>
2022-11-15 10:22:55 +0800

    dropdown html

--- javascript.js
+++ javascript.js
@@ -1,12 +1,39 @@
 
+var currently_editing = 0;
+const editing_list =["Body","Expressions"]
 
 var img_head = new Image();
 var img_body = new Image();
 img_head.src = "temp_images/sam_hair.png";
 img_body.src = "temp_images/sam_face.png";
 
-function setVariable(part, number){
-    if (part =="hair"){
+function setControls(number){
+    currently_editing = number
+    document.getElementById("editingTitle").innerHTML = editing_list[number];
+
+    drop_string = '<div class="dropdown">';
+    drop_string +='<button onclick="dropFunction()" class="dropbtn">Hair</button>';
+    drop_string +='<div id="myDropdown" class="dropdown-content">';
+    drop_string +='<button onclick="setVariable('+number+',0)" >zero</button>';
+    drop_string +='<button onclick="setVariable('+number+',1)" >one</button>';
+    drop_string +='</div></div>';
+    document.getElementById("controls").innerHTML = drop_string;
+}
+
+function makeDropbtn(name, variablename, list){
+    drop_string = '<div class="dropdown">';
+    drop_string +='<button onclick="dropFunction()" class="dropbtn">'+name+'</button>';
+    drop_string +='<div id="myDropdown" class="dropdown-content">';
+    for (row = 0; row < list.length; row += 1) {
+        drop_string +='<button onclick="setVariable('+variablename+','+row+')" >'+list[row]+'</button>';
+    }
+    drop_string +='</div></div>';
+    document.getElementById(variablename+"Btn").innerHTML = drop_string;
+
+}
+
+function setVariable(variable, number){
+    if (variable =="hair"){
         if (number==0){
             img_head.src = "temp_images/sam_hair.png";
         }
@@ -14,8 +41,10 @@ function setVariable(part, number){
             img_head.src = "temp_images/sam_hair2.png";
         }
     }
+    else{
+
+    }
     drawCanvas();
-    document.getElementById("controls").innerHTML = "Hello "+number; 
 }
 
 function drawCanvas() {
@@ -31,7 +60,12 @@ function drawCanvas() {
     }
 }
 
-window.onload = drawCanvas;
+function setup(){
+    makeDropbtn("Editing:", "currently_editing", editing_list);
+    drawCanvas();
+}
+
+window.onload = setup;
 
 // addbutton to cycle between colours
 // then turn it into popup window
