05fad3c298dac5091ddc517a126cd0bc407d7cf7
sqbr <sqbr@users.noreply.github.com>
2022-11-23 20:20:13 +0800

    all layers added

--- code/setup2.js
+++ code/setup2.js
@@ -35,9 +35,20 @@ sprite_pants_list.push({name: "Shorts", location: "pants/pants", colour: true, t
 const sprite_shirt_list = []
 sprite_shirt_list.push({name: "Overalls", location: "shirts/shirts", colour: false, topcorner:[0,0]})
 
+const sprite_accessory_list = []
+sprite_accessory_list.push({name: "Glasses", location: "accessory/accessories", colour: false, topcorner:[0,32]})
+
+const sprite_hair_list = []
+sprite_hair_list.push({name: "Side part", location: "hair/hairstyles", colour: true, topcorner:[32,0]})
+
 const sprite_hat_list = []
 sprite_hat_list.push({name: "Santa hat", location: "hats/hats", colour: false, topcorner:[23,159]})
 
+const sprite_arms_list = []
+for (let i =0; i<sprite_body_list.length;i++){
+    b= sprite_body_list[i];
+    sprite_arms_list.push({name: b, location: "body/farmer_"+b, colour: true, topcorner:[96,0]})
+}
 
 const sprite_objects =[];
 
@@ -50,7 +61,10 @@ add_sprite_object("Eyes", sprite_eyes_list,eye_colours,true,[16,32],[0,0]);
 add_sprite_object("Pants", sprite_pants_list,outfit_colours,true,[16,32],[0,0]);
 add_sprite_object("Shoes", sprite_shoes_list,outfit_colours,true,[16,32],[0,0]);
 add_sprite_object("Shirt", sprite_shirt_list,outfit_colours,false,[8,8],[4,15]);
-add_sprite_object("Hat", sprite_hat_list,outfit_colours,false,[16,20],[0,0]);
+add_sprite_object("Accessory", sprite_accessory_list,outfit_colours,false,[16,16],[0,1]);
+add_sprite_object("Hair", sprite_hair_list,hair_colours,false,[16,32],[0,1]);
+//add_sprite_object("Hat", sprite_hat_list,outfit_colours,false,[16,20],[0,0]);
+add_sprite_object("Arms", sprite_arms_list,skin_colours,true,[16,32],[0,0]);
 
 function print_sprite_objects(){
     s = "";
